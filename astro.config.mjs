7675bf604ffbc06c3397f884a5f42490682cadf3
Marek Vsechovsky <marek@vsechovsky.com>
2025-02-13 17:19:37 -0300

    Forgot to prefix the url with the protocol.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "docs.vsechovsky.com",
+	site: "http://docs.vsechovsky.com",
 	base: "/",
 	integrations: [
 		starlight({
